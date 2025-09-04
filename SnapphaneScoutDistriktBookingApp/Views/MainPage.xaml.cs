21eef8e7236ce0f6633d0be85b9060557f8c3c6a
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-09-04 10:56:00 +0200

    Minor code fixes.

--- SnapphaneScoutDistriktBookingApp/Views/MainPage.xaml.cs
+++ SnapphaneScoutDistriktBookingApp/Views/MainPage.xaml.cs
@@ -35,7 +35,7 @@ namespace SnapphaneScoutDistriktBookingApp
 
         }
 
-        private async void OnChangeToCanoe(object sender, EventArgs e)
+        private async void OnChangeToBookingSelect(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new BookingPage());
         }
