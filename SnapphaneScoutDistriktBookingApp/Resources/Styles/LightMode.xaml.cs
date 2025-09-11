8d54bab8f5a1574b1a413b6c0e784397ab003b6e
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-09-11 11:08:48 +0200

    Added button to switch app theme.

--- SnapphaneScoutDistriktBookingApp/Resources/Styles/LightMode.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace SnapphaneScoutDistriktBookingApp.Resources.Styles;
-
-public partial class LightMode : ResourceDictionary
-{
-	public LightMode()
-	{
-		InitializeComponent();
-	}
-}
--- SnapphaneScoutDistriktBookingApp/Views/MainPage.xaml.cs
+++ SnapphaneScoutDistriktBookingApp/Views/MainPage.xaml.cs
@@ -41,6 +41,19 @@ namespace SnapphaneScoutDistriktBookingApp
             }
         }
 
+        private void OnClickedChangeTheme(object sender, EventArgs e)
+        {
+            var app = Application.Current;
+            if (app.UserAppTheme == AppTheme.Light)
+            {
+                app.UserAppTheme = AppTheme.Dark;
+            }
+            else
+            {
+                app.UserAppTheme = AppTheme.Light;
+            }
+        }
+
         private async void OnChangeToBookingSelect(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new BookingPage());
